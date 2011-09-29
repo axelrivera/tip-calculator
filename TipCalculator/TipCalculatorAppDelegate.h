6b482ba978511ce378d715df61b74b7ce7b365a2
Axel Rivera <axel@riveralabs.com>
2011-09-29 10:39:32 -0400

    Initial Commit

---
+++ TipCalculator/TipCalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TipCalculatorAppDelegate.h
+//  TipCalculator
+//
+//  Created by arn on 9/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipCalculatorAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
